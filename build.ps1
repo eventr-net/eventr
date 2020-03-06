c93bb5d21626f1a007f684929ab1c8a3d92d03f2
Jan Dvorak <jan.dvorak@nanoenergies.cz>
2020-03-06 14:55:45 +0100

    updated shared-solution-files

--- build.ps1
+++ build.ps1
@@ -8,6 +8,7 @@ param (
     [switch] $RunTests,
     [switch] $CreatePackages,
     [switch] $PublishToNuGet,
+    [switch] $PublishLocally,
     [string] $NuGetApiKey
 )
 
@@ -15,7 +16,7 @@ param (
 
 Push-Location (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
-$SlnFile = 'eventr.sln'
+$SlnFile = Get-SolutionFile
 $CodeAnalysis = if ($SkipCodeAnalysis) {"False"} else {"True"}
 $Warnings = if ($WarningsAsErrors) {"True"} else {"False"}
 $Verbosity = if ($Verbose) {"normal"} else {"minimal"}
@@ -26,6 +27,7 @@ if ($ForceNugetPackagesRoot -and ($ForceNugetPackagesRoot -ne '')) {
 }
 
 # clean up
+Write-Label 'Cleaning solution directory'
 Clear-SolutionDirectory
 
 # display build info
@@ -38,20 +40,20 @@ Write-AdditionalAssemblyInfo
 Get-LastExecErrorAndExitIfExists 'The build has failed'
 
 # unit tests
-if ($RunTests -or $PublishToNuGet) {
+if ($RunTests -or $PublishToNuGet -or $PublishLocally) {
     Get-ChildItem './test' -Include '*.Tests.csproj' -Recurse | ForEach-Object {
         $projName = [System.IO.Path]::GetFileNameWithoutExtension($_.FullName)
-        Write-Label "Running unit tests $projName"
+        Write-Debug "Running unit tests $projName"
         & $global:DOTNET_PATH test $_.FullName -c $Configuration -v $Verbosity --no-build --no-restore /nologo
         Get-LastExecErrorAndExitIfExists "One or more tests have failed while running $projName"
     }
 }
 
 # create packages
-if ($CreatePackages -or $PublishToNuGet) {    
+if ($CreatePackages -or $PublishToNuGet -or $PublishLocally) {    
     Get-ChildItem './src' -Include '*.csproj' -Recurse | ForEach-Object {
         $projName = [System.IO.Path]::GetFileNameWithoutExtension($_.FullName)
-        Write-Label "Packaging $projName"
+        Write-Debug "Packaging $projName"
         & $global:DOTNET_PATH pack $_.FullName -c $Configuration -o $global:PUBLISH_DIR -v $Verbosity --no-build --no-restore /p:Version=$Version /nologo
         Get-LastExecErrorAndExitIfExists "Failed to package $projName"
     }    
@@ -59,7 +61,13 @@ if ($CreatePackages -or $PublishToNuGet) {
 
 # publish
 if ($PublishToNuGet) {
+    Write-Label 'Publishing packages to NuGet'
     Publish-NuGetPackages
+}
+
+if ($PublishLocally) {
+    Write-Label 'Publishing packages to local NuGet repository'
+    Publish-NuGetPackagesLocally
 }
 
 Pop-Location
